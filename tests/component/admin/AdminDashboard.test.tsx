8de2b2b51f0bb5e79c137ea0c427aaabd08d4413
Gal Grinblat <galgr1@gmail.com>
2026-03-03 21:09:24 +0200

    fix failing test

--- tests/component/admin/AdminDashboard.test.tsx
+++ tests/component/admin/AdminDashboard.test.tsx
@@ -19,6 +19,8 @@ jest.mock('../../../src/contexts/AdminDataContext', () => ({
     loadPlayers: jest.fn(),
     players: [],
     isLoadingPlayers: false,
+    users: [],
+    isLoadingUsers: false,
   }),
 }));
 
